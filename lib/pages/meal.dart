927e4c5ab3724f57b17c67aab1a9f46550a8c945
Carlos Perez <carlosperezmolano@gmail.com>
2020-06-02 15:05:16 +0200

    Add image picker support

--- lib/pages/meal.dart
+++ lib/pages/meal.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
 
 class Meal extends StatefulWidget {
   @override
@@ -7,10 +8,30 @@ class Meal extends StatefulWidget {
 }
 
 class _MealState extends State<Meal> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals"),
+      ),
+      body: Center(
+        child: _image == null ? Text("empty image") : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: Icon(Icons.add_a_photo)),
     );
   }
 }
