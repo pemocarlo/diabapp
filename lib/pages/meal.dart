02571e89468dd13aab57b8d8d17c9dc3356df6e9
Carlos Perez <carlosperezmolano@gmail.com>
2020-06-03 16:12:39 +0200

    Add menu widget

--- lib/pages/meal.dart
+++ lib/pages/meal.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:diabapp/widgets/menu.dart';
 
 class Meal extends StatefulWidget {
   @override
@@ -22,12 +23,21 @@ class _MealState extends State<Meal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff392850),
       appBar: AppBar(
         title: Text("Meals"),
       ),
-      body: Center(
-        child: _image == null ? Text("empty image") : Image.file(_image),
-      ),
+      body: Stack(children: <Widget>[
+        Center(
+          child: _image == null
+              ? Text(
+                  "empty image",
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                )
+              : Image.file(_image),
+        ),
+        Menu(),
+      ]),
       floatingActionButton: FloatingActionButton(
           onPressed: getImage,
           tooltip: 'Pick Image',
