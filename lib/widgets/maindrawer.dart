59265411c051574c763af709f844a238edbc521b
Carlos Perez <carlosperezmolano@gmail.com>
2020-06-24 14:25:13 +0200

    Refactor drawer

--- lib/widgets/maindrawer.dart
+++ lib/widgets/maindrawer.dart
@@ -1,8 +1,5 @@
-import 'package:diabapp/data/open_food_facts_database.dart';
 import 'package:flutter/material.dart';
 import 'package:diabapp/widgets/index.dart';
-import 'package:provider/provider.dart';
-import 'package:moor_db_viewer/moor_db_viewer.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({
@@ -18,55 +15,89 @@ class MainDrawer extends StatelessWidget {
       // Add a ListView to the drawer. This ensures the user can scroll
       // through the options in the drawer if there isn't enough vertical
       // space to fit everything.
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
+      child: Column(
         children: <Widget>[
-          DrawerHeader(
-            child: Text('DiabApp'),
-            decoration: BoxDecoration(
-              color: Colors.blue,
+          Expanded(
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('DiabApp'),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.dashboard),
+                  title: Text("Dashboard",
+                      style: TextStyle(color: Colors.black, fontSize: 18)),
+                  onTap: () {
+                    onTap(DashBoard(), context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.data_usage),
+                  title: Text(
+                    'Graphs',
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  onTap: () {
+                    onTap(Graphs(), context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.schedule),
+                  title: Text("Schedule",
+                      style: TextStyle(color: Colors.black, fontSize: 18)),
+                  onTap: () {
+                    onTap(Schedule(), context);
+                  },
+                ),
+                // ListTile(
+                //   title: Text("Database viewer",
+                //       style: TextStyle(color: Colors.black, fontSize: 18)),
+                //   onTap: () {
+                //     final database =
+                //         Provider.of<OpenFoodFactsDataBase>(context, listen: false);
+                //     Navigator.of(context).push(MaterialPageRoute(
+                //         builder: (context) => MoorDbViewer(database)));
+                //   },
+                // ),
+                ListTile(
+                  leading: Icon(Icons.fastfood),
+                  title: Text("New meal",
+                      style: TextStyle(color: Colors.black, fontSize: 18)),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/search");
+                  },
+                ),
+              ],
             ),
           ),
-          ListTile(
-            title: Text("Dashboard",
-                style: TextStyle(color: Colors.black, fontSize: 18)),
-            onTap: () {
-              onTap(DashBoard(), context);
-            },
-          ),
-          ListTile(
-            title: Text(
-              'Graphs',
-              style: TextStyle(color: Colors.black, fontSize: 18),
+          // This container holds the align
+          Container(
+            // This align moves the children to the bottom
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              // This container holds all the children that will be aligned
+              // on the bottom and should not scroll with the above ListView
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Divider(),
+                    ListTile(
+                      leading: Icon(Icons.settings),
+                      title: Text('Settings'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.help),
+                      title: Text('About'),
+                    )
+                  ],
+                ),
+              ),
             ),
-            onTap: () {
-              onTap(Graphs(), context);
-            },
-          ),
-          ListTile(
-            title: Text("Schedule",
-                style: TextStyle(color: Colors.black, fontSize: 18)),
-            onTap: () {
-              onTap(Schedule(), context);
-            },
-          ),
-          // ListTile(
-          //   title: Text("Database viewer",
-          //       style: TextStyle(color: Colors.black, fontSize: 18)),
-          //   onTap: () {
-          //     final database =
-          //         Provider.of<OpenFoodFactsDataBase>(context, listen: false);
-          //     Navigator.of(context).push(MaterialPageRoute(
-          //         builder: (context) => MoorDbViewer(database)));
-          //   },
-          // ),
-          ListTile(
-            title: Text("New meal",
-                style: TextStyle(color: Colors.black, fontSize: 18)),
-            onTap: () {
-              Navigator.pushNamed(context, "/search");
-            },
           ),
         ],
       ),
